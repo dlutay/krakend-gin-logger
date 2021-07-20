ef96a4955b588e7683a0faa5385987c776b49068
d.lutay <d.lutay@artox.com>
2021-07-20 17:46:14 +0300

    change config types

--- logger.go
+++ logger.go
@@ -11,8 +11,8 @@ import (
 
 	"github.com/devopsfaith/krakend-gologging"
 	logstash "github.com/devopsfaith/krakend-logstash"
-	"github.com/devopsfaith/krakend/config"
-	"github.com/devopsfaith/krakend/logging"
+	"github.com/luraproject/lura/config"
+	"github.com/luraproject/lura/logging"
 	"github.com/gin-gonic/gin"
 )
 
